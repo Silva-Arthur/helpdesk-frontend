ad617c1e854f1e8dc82ca3163b125bf195ccdee4
Arthur <pereiras.oarthur@gmail.com>
2023-09-07 13:10:42 -0300

    Rota para componente Tecnico-Create

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { TecnicoCreateComponent } from './components/tecnico/tecnico-create/tecnico-create.component';
 import { LoginComponent } from './components/login/login.component';
 import { TecnicoListComponent } from './components/tecnico/tecnico-list/tecnico-list.component';
 import { HomeComponent } from './components/home/home.component';
@@ -14,7 +15,9 @@ const routes: Routes = [
     canActivate: [AuthGuard], //guardião das rotas, para verificar se o token é válido ou não
     children: [ // rotas filhas
       {path: 'home', component: HomeComponent},
-      {path: 'tecnicos', component: TecnicoListComponent}
+
+      {path: 'tecnicos', component: TecnicoListComponent},
+      {path: 'tecnicos/create', component: TecnicoCreateComponent}
     ]
   }
 ];
