105790114ee3ec399443f8f43d2f8a92778457d2
Arthur <pereiras.oarthur@gmail.com>
2023-09-07 11:16:36 -0300

    Serviço de mensagens implenetado

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Credenciais } from './../../models/credenciais';
 import { Component, OnInit } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-login',
@@ -17,13 +18,18 @@ export class LoginComponent implements OnInit {
   email = new FormControl(null, Validators.email);
   senha = new FormControl(null, Validators.minLength(3));
 
-  constructor() { }
+  constructor(private toast: ToastrService) { }
 
   ngOnInit(): void {
   }
 
   validaCampos(): boolean {
     return (this.email.valid && this.senha.valid);
+  }
+
+  logar() {
+    this.toast.error('Usuário e/ou Senha inválidos!', 'Login');
+    this.creds.senha = '';
   }
 
 }
