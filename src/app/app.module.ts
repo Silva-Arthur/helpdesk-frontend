c949e8e08e0402f29492b5526bb8b32004817bb6
Arthur <pereiras.oarthur@gmail.com>
2023-09-08 21:09:46 -0300

    Componente Chamado-Create criado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -44,6 +44,7 @@ import { NgxMaskModule } from 'ngx-mask';
 import { TecnicoUpdateComponent } from './components/tecnico/tecnico-update/tecnico-update.component';
 import { TecnicoDeleteComponent } from './components/tecnico/tecnico-delete/tecnico-delete.component';
 import { ChamadoListComponent } from './components/chamado/chamado-list/chamado-list.component';
+import { ChamadoCreateComponent } from './components/chamado-create/chamado-create.component';
 
 @NgModule({
   declarations: [
@@ -60,7 +61,8 @@ import { ChamadoListComponent } from './components/chamado/chamado-list/chamado-
     ClienteDeleteComponent,
     ClienteListComponent,
     ClienteUpdateComponent,
-    ChamadoListComponent
+    ChamadoListComponent,
+    ChamadoCreateComponent
   ],
   imports: [
     BrowserModule,
